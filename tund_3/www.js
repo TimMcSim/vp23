d58a73f296a5517ac83fb5484741b03cc2f6ad61
Maksim Arfanov <timmcsim@users.noreply.github.com>
2023-09-28 15:23:43 +0300

    Esimesed 3 tundi

---
+++ tund_3/www.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+http.createServer(function(req, res){
+	res.writeHead(200, {"Content-type": "text/html"});
+	res.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Andrus Rinde, veebiprogrammeerimine 2023</title><script src="first.js" defer></script></head><body>');
+	res.write('<h1>Andrus Rinde</h1><p>See veebileht on valminud <a href="https://www.tlu.ee" target="_blank">TLÜ</a> Digitehnoloogiate instituudi informaatia eriala õppetöö raames.</p>');
+	res.write('<hr></body></html>');
+	console.log('Keegi vaatab!');
+	//valmis, saada ara
+	return res.end();
+}).listen(5107);
+
+//rinde 5100
+//mina 5107
+
